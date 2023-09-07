e7ae117cfa250e99ca24fdb9947472b821b0836c
GeroPazPapa <geronimonicolaspazpapa@gmail.com>
2023-09-07 11:46:47 -0300

    Vercel2

--- Server/src/controllers/getAllUsersController.js
+++
@@ -1,8 +0,0 @@
-const { User } = require("../db");
-
-const getAllUsersController = async () => {
-  const users = await User.findAll();
-  return users
-}
-
-module.exports = { getAllUsersController };
