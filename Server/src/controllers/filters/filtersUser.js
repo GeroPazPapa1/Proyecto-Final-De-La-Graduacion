c4ec91247f1c2d9c8ff50485cfa2174d598430f6
AMendozaMarimon <124471764+AMendozaMarimon@users.noreply.github.com>
2023-09-10 09:22:05 -0500

    SearchBar Useer funcional

---
+++ Server/src/controllers/filters/filtersUser.js
@@ -0,0 +1,63 @@
+const { User } = require ("../../db");
+const { Op } = require('sequelize');
+
+
+const filteredUsers = async (age, country, email, status, ban, verify, lastName, name) => {
+    try {const conditions = {};
+
+    if (age) {
+        conditions.age = age
+    }
+  
+    if (country) {
+        conditions.country = {
+        [Op.iLike]: `%${country}%`,
+        }
+    }
+
+    if (status === 'admin' || status === 'user') {
+        conditions.status = status;
+      }
+
+    
+     if (name) {
+        conditions[Op.or] = [
+          {
+            name: {
+              [Op.iLike]: `%${name}%`, 
+            },
+          },
+          {
+            lastName: {
+              [Op.iLike]: `%${name}`,
+            },
+          },
+          {
+            email: {
+              [Op.iLike]: `%${name}`,
+            },
+          },
+        ]
+      }
+
+    if (typeof ban === 'boolean') {
+        conditions.ban = ban;
+    }
+
+    if (typeof verify === 'boolean') {
+        conditions.verify = verify;
+    }
+
+      console.log(conditions);
+    
+    const filtersUser = await User.findAll({
+      where: conditions,
+    })
+    return filtersUser;
+    } catch (error) {
+        return error.message;
+    }
+}
+
+module.exports = {
+    filteredUsers,}
