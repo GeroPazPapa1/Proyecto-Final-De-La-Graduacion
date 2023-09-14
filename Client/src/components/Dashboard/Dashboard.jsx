188985c3abbac0418ddbd7291acf7ee8c7471648
AMendozaMarimon <124471764+AMendozaMarimon@users.noreply.github.com>
2023-09-13 19:14:26 -0500

    Cambios solicitados al AboutUs

--- Client/src/components/Dashboard/Dashboard.jsx
+++ Client/src/components/Dashboard/Dashboard.jsx
@@ -64,7 +64,7 @@ export default function Dashboard() {
                                     </div>
                                     <div>
                                         <Link to="/home">
-                                            <button>COME HOME</button>
+                                            <button>GO HOME</button>
                                         </Link>
                                     </div>
                                 </div>
@@ -123,7 +123,7 @@ export default function Dashboard() {
                                     </div>
                                     <div>
                                         <Link to="/home">
-                                            <button>COME HOME</button>
+                                            <button>GO HOME</button>
                                         </Link>
                                     </div>
                                 </div>
