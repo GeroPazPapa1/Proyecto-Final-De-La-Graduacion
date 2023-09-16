3add0faadcc982d8a86f7e51baf79548dc729d95
Daniel-Botache <daniel.botache@gmail.com>
2023-09-16 17:08:40 -0500

    se mejoran los estilos para la dashboard de usuario

--- Client/src/components/NavBar/NavBar.jsx
+++ Client/src/components/NavBar/NavBar.jsx
@@ -63,15 +63,27 @@ export default function NavBar() {
             </>
           ) : (
             <>
-              <Link to={"/profileSettings"} className={styles.icon_name_user}>
-                <img src={user.image} alt="" className={styles.iconImage} />{" "}
-                <button className={styles.button} id={loggedUser.id}>
-                  {user.name}
-                </button>
-              </Link>
+              <div className={styles.dropdownContainer}>
+                <div className={styles.dropdown}>
+                  <div className={styles.icon_name_user}>
+                    <img src={user.image} alt="" className={styles.iconImage} />{" "}
+                    <button className={styles.buttonDropdown}>
+                      {user.name} &#9660;{" "}
+                      {/* Agregamos una flecha hacia abajo para indicar que es desplegable */}
+                    </button>
+                  </div>
+                  <div className={styles.dropdownContent}>
+                    {/* Aquí agregamos las opciones del menú */}
+                    <Link to="/purchases">Purchases</Link>
+                    <Link to="/profile">My Profile</Link>
+                    <Link to="/reviews">Reviews</Link>
+                  </div>
+                </div>
+              </div>
+
               {loggedUser.response?.type === "admin" && (
                 <Link to={"/admin/dashboard"}>
-                  <button>Dashboard</button>
+                  <button className={styles.button}>Dashboard</button>
                 </Link>
               )}
               <Link to={"/"}>
@@ -100,15 +112,27 @@ export default function NavBar() {
             <div className={styles.containerL}>
               {loggedUser && (
                 <>
-                  <Link
-                    to={"/profileSettings"}
-                    className={styles.icon_name_user}
-                  >
-                    <img src={user.image} alt="" className={styles.iconImage} />{" "}
-                    <button className={styles.button} id={loggedUser.id}>
-                      {user.name}
-                    </button>
-                  </Link>
+                  <div className={styles.dropdownContainer}>
+                    <div className={styles.dropdown}>
+                      <div className={styles.icon_name_user}>
+                        <img
+                          src={user.image}
+                          alt=""
+                          className={styles.iconImage}
+                        />{" "}
+                        <button className={styles.buttonDropdown}>
+                          {user.name} &#9660;{" "}
+                          {/* Agregamos una flecha hacia abajo para indicar que es desplegable */}
+                        </button>
+                      </div>
+                      <div className={styles.dropdownContent}>
+                        {/* Aquí agregamos las opciones del menú */}
+                        <Link to="/purchases">Purchases</Link>
+                        <Link to="/profile">My Profile</Link>
+                        <Link to="/reviews">Reviews</Link>
+                      </div>
+                    </div>
+                  </div>
                   {loggedUser.response?.type === "admin" && (
                     <Link to={"/admin/dashboard"}>
                       <button className={styles.button}>Dashboard</button>
@@ -156,20 +180,32 @@ export default function NavBar() {
               {loggedUser && (
                 <>
                   <span>{loggedUser.email}</span>
+                  <div className={styles.dropdownContainer}>
+                    <div className={styles.dropdown}>
+                      <div className={styles.icon_name_user}>
+                        <img
+                          src={user.image}
+                          alt=""
+                          className={styles.iconImage}
+                        />{" "}
+                        <button className={styles.buttonDropdown}>
+                          {user.name} &#9660;{" "}
+                          {/* Agregamos una flecha hacia abajo para indicar que es desplegable */}
+                        </button>
+                      </div>
+                      <div className={styles.dropdownContent}>
+                        {/* Aquí agregamos las opciones del menú */}
+                        <Link to="/purchases">Purchases</Link>
+                        <Link to="/profile">My Profile</Link>
+                        <Link to="/reviews">Reviews</Link>
+                      </div>
+                    </div>
+                  </div>
                   {loggedUser.response?.type === "admin" && (
                     <Link to={"/admin/dashboard"}>
                       <button>Dashboard</button>
                     </Link>
                   )}
-                  <Link
-                    to={"/profileSettings"}
-                    className={styles.icon_name_user}
-                  >
-                    <img src={user.image} alt="" className={styles.iconImage} />{" "}
-                    <button className={styles.button} id={loggedUser.id}>
-                      {user.name}
-                    </button>
-                  </Link>
                   <Link to={"/"}>
                     <button onClick={handleLogout} className={styles.button}>
                       Log out
@@ -211,18 +247,98 @@ export default function NavBar() {
                   <span>{loggedUser.email}</span>
                   {loggedUser.response?.type === "admin" && (
                     <Link to={"/admin/dashboard"}>
+                      <button className={styles.button}>Dashboard</button>
+                    </Link>
+                  )}
+                  <div className={styles.dropdownContainer}>
+                    <div className={styles.dropdown}>
+                      <div className={styles.icon_name_user}>
+                        <img
+                          src={user.image}
+                          alt=""
+                          className={styles.iconImage}
+                        />{" "}
+                        <button className={styles.buttonDropdown}>
+                          {user.name} &#9660;{" "}
+                          {/* Agregamos una flecha hacia abajo para indicar que es desplegable */}
+                        </button>
+                      </div>
+                      <div className={styles.dropdownContent}>
+                        {/* Aquí agregamos las opciones del menú */}
+                        <Link to="/purchases">Purchases</Link>
+                        <Link to="/profile">My Profile</Link>
+                        <Link to="/reviews">Reviews</Link>
+                      </div>
+                    </div>
+                  </div>
+                  <Link to={"/"}>
+                    <button onClick={handleLogout} className={styles.button}>
+                      Log out
+                    </button>
+                  </Link>
+                </>
+              )}
+              {!loggedUser && (
+                <>
+                  <Link to={"/login"}>
+                    <button className={styles.button}>Log in</button>
+                  </Link>
+                  <Link to={"/register"}>
+                    <button className={styles.button}>Register</button>
+                  </Link>
+                </>
+              )}
+              <Link to="/cart">
+                <img className={styles.icons} src={CART} alt="Cart..." />
+              </Link>
+            </div>
+          </div>
+        </>
+      )}
+      {location.pathname === "/profile" && (
+        <>
+          <div className={styles.containerLP}>
+            <div>
+              <Link to={"/home"}>
+                <button className={styles.button}>Home</button>
+              </Link>
+              <button className={styles.button}>Contacts</button>
+              <Link to={"/favorites"}>
+                <button className={styles.button}>Favorites</button>
+              </Link>
+            </div>
+          </div>
+          <div>
+            <div className={styles.containerL}>
+              {loggedUser && (
+                <>
+                  <span>{loggedUser.email}</span>
+                  <div className={styles.dropdownContainer}>
+                    <div className={styles.dropdown}>
+                      <div className={styles.icon_name_user}>
+                        <img
+                          src={user.image}
+                          alt=""
+                          className={styles.iconImage}
+                        />{" "}
+                        <button className={styles.buttonDropdown}>
+                          {user.name} &#9660;{" "}
+                          {/* Agregamos una flecha hacia abajo para indicar que es desplegable */}
+                        </button>
+                      </div>
+                      <div className={styles.dropdownContent}>
+                        {/* Aquí agregamos las opciones del menú */}
+                        <Link to="/purchases">Purchases</Link>
+                        <Link to="/profile">My Profile</Link>
+                        <Link to="/reviews">Reviews</Link>
+                      </div>
+                    </div>
+                  </div>
+                  {loggedUser.response?.type === "admin" && (
+                    <Link to={"/admin/dashboard"}>
                       <button>Dashboard</button>
                     </Link>
                   )}
-                  <Link
-                    to={"/profileSettings"}
-                    className={styles.icon_name_user}
-                  >
-                    <img src={user.image} alt="" className={styles.iconImage} />{" "}
-                    <button className={styles.button} id={loggedUser.id}>
-                      {user.name}
-                    </button>
-                  </Link>
                   <Link to={"/"}>
                     <button onClick={handleLogout} className={styles.button}>
                       Log out
--- Client/src/components/ProfileDashboard/ProfileDashboard.jsx
+++ Client/src/components/ProfileDashboard/ProfileDashboard.jsx
@@ -1,73 +1,96 @@
 import React, { useState } from "react";
-import styles from './ProfileDashboard.module.css'
-import Profile from './Profile/Profile'
-import Posts from './Posts/Posts'
-import Reviews from './Reviews/Reviews'
-import CartHistory from './CartHistory/CartHistory'
+import styles from "./ProfileDashboard.module.css";
+import Posts from "./Posts/Posts";
+import Reviews from "./Reviews/Reviews";
+import CartHistory from "./CartHistory/CartHistory";
+import Modification from "../Modification/Modification";
 
 export default function ProfileDashboard() {
-    const [selectedTopic, setSelectedTopic] = useState("Cart History")
+  const [selectedTopic, setSelectedTopic] = useState("Cart History");
 
-    const handleTopic = (topic) => {
-        setSelectedTopic(topic)
-    }
+  const handleTopic = (topic) => {
+    setSelectedTopic(topic);
+  };
 
-    return (
-        <>
-            <div className={styles.profile}>
-                <div className={styles.col1}>
-                    <div className={styles.topics}>
-                        <button
-                            className={selectedTopic === "Profile" ? styles.selectedTopic : styles.topic}
-                            onClick={() => handleTopic("Profile")}
-                        >
-                            Profile
-                        </button>
-                        <button
-                            className={selectedTopic === "Cart History" ? styles.selectedTopic : styles.topic}
-                            onClick={() => handleTopic("Cart History")}
-                        >
-                            Cart History
-                        </button>
-                        <button
-                            className={selectedTopic === "Posts" ? styles.selectedTopic : styles.topic}
-                            onClick={() => handleTopic("Posts")}
-                        >
-                            Posts
-                        </button>
-                        <button
-                            className={selectedTopic === "Reviews" ? styles.selectedTopic : styles.topic}
-                            onClick={() => handleTopic("Reviews")}
-                        >
-                            Reviews
-                        </button>
-                    </div>
+  return (
+    <>
+      <div className={styles.profile}>
+        <div className={styles.col1}>
+          <div className={styles.topics}>
+            <button
+              className={
+                selectedTopic === "Profile"
+                  ? styles.selectedTopic
+                  : styles.topic
+              }
+              onClick={() => handleTopic("Profile")}
+            >
+              <img
+                src="https://img.icons8.com/ios/50/paid--v1.png"
+                alt="Cart icon"
+                className={styles.img}
+              />
+              Purchases
+            </button>
+            <button
+              className={
+                selectedTopic === "Cart History"
+                  ? styles.selectedTopic
+                  : styles.topic
+              }
+              onClick={() => handleTopic("Cart History")}
+            >
+              <img
+                src="https://img.icons8.com/material-outlined/24/admin-settings-male.png"
+                alt="Profile Icon"
+                className={styles.img}
+              />
+              My profile
+            </button>
+            <button
+              className={
+                selectedTopic === "Reviews"
+                  ? styles.selectedTopic
+                  : styles.topic
+              }
+              onClick={() => handleTopic("Reviews")}
+            >
+              <img
+                src="https://img.icons8.com/external-xnimrodx-lineal-xnimrodx/64/external-rating-ecommerce-xnimrodx-lineal-xnimrodx-3.png"
+                alt="Review icon"
+                className={styles.img}
+              />
+              Reviews
+            </button>
+          </div>
+        </div>
+        <div className={styles.col2}>
+          <div className={styles.col2a}>
+            {selectedTopic === "Profile" && (
+              <div className={styles.profile}>
+                <div className={styles.carthistory}>
+                  <CartHistory />
                 </div>
-                <div className={styles.col2}>
-                    <div className={styles.col2a}>
-                        {selectedTopic === "Profile" && (
-                            <div className={styles.profile}>
-                                <Profile />
-                            </div>
-                        )}
-                        {selectedTopic === "Cart History" && (
-                            <div className={styles.carthistory}>
-                                <CartHistory />
-                            </div>
-                        )}
-                        {selectedTopic === "Posts" && (
-                            <div className={styles.posts}>
-                                <Posts />
-                            </div>
-                        )}
-                        {selectedTopic === "Reviews" && (
-                            <div className={styles.reviews}>
-                                <Reviews />
-                            </div>
-                        )}
-                    </div>
-                </div>
-            </div>
-        </>
-    )
+              </div>
+            )}
+            {selectedTopic === "Cart History" && (
+              <div className={styles.myProfile}>
+                <Modification />
+              </div>
+            )}
+            {selectedTopic === "Posts" && (
+              <div className={styles.posts}>
+                <Posts />
+              </div>
+            )}
+            {selectedTopic === "Reviews" && (
+              <div className={styles.reviews}>
+                <Reviews />
+              </div>
+            )}
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
