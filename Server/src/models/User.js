e11fb0031e4cd2946db08fb53333d2617bc8da5b
Ema0502 <ema050296@gmail.com>
2023-09-18 11:20:03 -0300

    correcciones

--- Server/src/models/User.js
+++ Server/src/models/User.js
@@ -33,7 +33,12 @@ module.exports = (sequelize) => {
       type: DataTypes.ENUM("admin", "user"),
       allowNull: false,
   },
-    image: { type: DataTypes.STRING, allowNull: true },
+  image: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    defaultValue:
+      "https://res.cloudinary.com/vehibuy/image/upload/q_auto/v1694066777/user_imgProfileStandar/istockphoto-903053114-612x612_qslxrv.webp",
+  },
     tel: { type: DataTypes.INTEGER, allowNull: true },
     ban: {
       type: DataTypes.BOOLEAN,
