42ed1955bfd28feaa0d1d371108b6c7b00d0dbd8
AMendozaMarimon <124471764+AMendozaMarimon@users.noreply.github.com>
2023-09-19 18:15:29 -0500

    Commit BD

--- Server/src/db.js
+++ Server/src/db.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME, DB_PORT } = process.env;
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
@@ -10,7 +10,7 @@ const path = require("path");
 
 //conexión de sequelize
 const sequelize = new Sequelize(
-  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  `postgresql://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`,
   { logging: false, native: false }
 );
 const basename = path.basename(__filename);
