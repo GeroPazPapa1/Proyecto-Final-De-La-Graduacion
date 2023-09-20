2c63835a2a507eefdd0e49141b489471cef1cf7c
Daniel-Botache <daniel.botache@gmail.com>
2023-09-20 10:54:00 -0500

    Merge branch 'Dev-Desarrollo' of https://github.com/GeroPazPapa1/Proyecto-Final-Final

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -16,7 +16,7 @@ import Register from "./components/LoginRegister/Register";
 import Favorites from "./components/Favorites/Favorites";
 import ProfileDashboard from "./components/ProfileDashboard/ProfileDashboard.jsx";
 import axios from "axios";
-axios.defaults.baseURL = "http://localhost:3001/";
+axios.defaults.baseURL = "http://localhost:7183/";
 
 function App() {
   return (
